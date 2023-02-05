6137d63775d202feb2fb3c0dbfede1b4de6b57df
Advaith07 <114658826+Advaith07@users.noreply.github.com>
2023-02-05 17:58:10 +0530

    Create peak_element_using_binarysearch.cpp

---
+++ peak_element_using_binarysearch.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) 
+    {
+        int n=nums.size();
+        if(n==1)
+            return 0;
+        int start=0,end=n-1;
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            if(mid>0 && mid<n-1)
+            {
+                if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
+                {
+                    return mid;
+                }
+                else if(nums[mid-1]>nums[mid])
+                {
+                    end=mid-1;
+                }
+                else
+                {
+                   start=mid+1;
+                }
+            }
+            else if(mid==0)
+            {
+                if(nums[0]>nums[1])
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            else if(mid==n-1)
+            {
+                if(nums[n-1]>nums[n-2])
+                {
+                    return n-1;
+                }
+                else
+                {
+                    return n-2;
+                }
+            }
+        }
+        return -1;
+    }
+};
